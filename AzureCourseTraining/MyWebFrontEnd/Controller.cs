7d810cd2e9cd1e18854c318e0cebc1814299fbe0
Bjorn Beek, van der <bjorn.van.der.beek@vecozo.nl>
2020-03-27 16:42:49 +0100

    add swagger api

---
+++ AzureCourseTraining/MyWebFrontEnd/Controller.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Queue;
+
+namespace MyWebFrontEnd
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Controller : ControllerBase
+    {
+        private const string connectionString =
+           "DefaultEndpointsProtocol=https;AccountName=bjornosstorageaccount;AccountKey=o2i2VBJ3z2zsezMW61SMNj3cbW61gnEuSFP9yrkqIgJnY97uwbpa6iB8vGVdAUdPGaZ/55+mZswmqb4qzE1imA==;EndpointSuffix=core.windows.net";
+
+        /// <summary>
+        /// Send firstname lastname
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> SetNameToQueue(string firstname, string lastname)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+            CloudQueue queue = queueClient.GetQueueReference("bjornosqueue");
+
+            await SendMessageAsync(queue, firstname + "" + lastname);
+
+            return Accepted();
+        }
+
+        private static async Task SendMessageAsync(CloudQueue theQueue, string newMessage)
+        {
+            bool createdQueue = await theQueue.CreateIfNotExistsAsync();
+
+            if (createdQueue)
+            {
+                Console.WriteLine("The queue was created.");
+            }
+
+            CloudQueueMessage message = new CloudQueueMessage(newMessage);
+            await theQueue.AddMessageAsync(message);
+        }
+    }
+}
--- AzureCourseTraining/MyWebFrontEnd/Startup.cs
+++ AzureCourseTraining/MyWebFrontEnd/Startup.cs
@@ -1,14 +1,13 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 namespace MyWebFrontEnd
 {
@@ -26,13 +25,40 @@ namespace MyWebFrontEnd
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                // This lambda determines whether user consent for non-essential cookies is needed
+                // for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "ToDo API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Shayne Boyer",
+                        Email = string.Empty,
+                        Url = new Uri("https://twitter.com/spboyer"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +79,16 @@ namespace MyWebFrontEnd
             app.UseCookiePolicy();
 
             app.UseMvc();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger
+            // JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
         }
     }
 }
